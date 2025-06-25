a884f1a40f7ced99c65d1bffabb9d2d0058d2399
Cherkasov Denis <denchik.cherkasov@gmail.com>
2025-06-25 19:54:50 +0700

    Выполненная вторая часть

--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
-//import styles from './ingredient-details.module.css';
+import styles from './ingredient-details.module.css';
+import { ingredientPropType } from '@utils/prop-types.js';
 
 export const IngredientDetails = ({ item }) => {
 	if (!item) return null;
 
 	return (
-		<div>
-			<img src={item.image_large} alt={item.name} className='mb-4' />
-			<p className='text text_type_main-medium mb-4'>{item.name}</p>
-			<ul>
-				<li>
-					Калории, ккал <span>{item.calories}</span>
-				</li>
-				<li>
-					Белки, г <span>{item.proteins}</span>
-				</li>
-				<li>
-					Жиры, г <span>{item.fat}</span>
-				</li>
-				<li>
-					Углеводы, г <span>{item.carbohydrates}</span>
-				</li>
-			</ul>
+		<div className={styles.content}>
+			<img src={item.image_large} alt={item.name} className={styles.image} />
+			<p className={`text text_type_main-medium ${styles.name}`}>{item.name}</p>
+			<div className={styles.nutrients}>
+				<div className={styles.nutrient}>
+					<span className='text text_type_main-default'>Калории, ккал</span>
+					<span className={`text text_type_digits-default ${styles.value}`}>
+						{item.calories}
+					</span>
+				</div>
+				<div className={styles.nutrient}>
+					<span className='text text_type_main-default'>Белки, г</span>
+					<span className={`text text_type_digits-default ${styles.value}`}>
+						{item.proteins}
+					</span>
+				</div>
+				<div className={styles.nutrient}>
+					<span className='text text_type_main-default'>Жиры, г</span>
+					<span className={`text text_type_digits-default ${styles.value}`}>
+						{item.fat}
+					</span>
+				</div>
+				<div className={styles.nutrient}>
+					<span className='text text_type_main-default'>Углеводы, г</span>
+					<span className={`text text_type_digits-default ${styles.value}`}>
+						{item.carbohydrates}
+					</span>
+				</div>
+			</div>
 		</div>
 	);
+};
+
+IngredientDetails.propTypes = {
+	item: ingredientPropType.isRequired,
 };
--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-//import styles from './order-details.module.css';
+import styles from './order-details.module.css';
 
 export const OrderDetails = () => {
 	return (
-		<div className='pt-10 pb-10 text-center'>
+		<div className={styles.container}>
 			<p className='text text_type_digits-large mb-8'>034536</p>
 			<p className='text text_type_main-medium mb-15'>идентификатор заказа</p>
+			<img src='/done.png' alt='Заказ принят' className={styles.icon} />
 			<p className='text text_type_main-default mb-2'>
 				Ваш заказ начали готовить
 			</p>
